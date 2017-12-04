99993d6bc48ded946388b67ef48f317bd41ea150
Arno Broeders <ahc.broeders@avans.nl>
2017-12-04 11:58:30 +0100

    Wijziging van Julian toegevoegd

--- src/nl/avans/Main.java
+++ src/nl/avans/Main.java
@@ -4,6 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Work in progress ... :)");
+        System.out.println("Work is done ... :)");
+        sayHello();
     }
 
     public static void sayHello() {
