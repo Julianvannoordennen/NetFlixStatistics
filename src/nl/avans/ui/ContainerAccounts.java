6527bf4111a1c29fffc80494b0f5604b745d9e38
Julian van Noordennen <jnoorden@avans.nl>
2018-01-08 14:16:02 +0100

    UI Resources toegevoegd, menu werkt

---
+++ src/nl/avans/ui/ContainerAccounts.java
@@ -0,0 +1,15 @@
+package nl.avans.ui;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class ContainerAccounts extends ContainerContent {
+
+    public ContainerAccounts() {
+        super("Accounts");
+        this.createComponents();
+    }
+
+    private void createComponents() {
+    }
+}
